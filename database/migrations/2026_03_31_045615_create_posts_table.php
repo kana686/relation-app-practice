687bf4e767a03bf8a76d24465d4324ec676101b1
kana686 <inui.k.w6@gmail.com>
2026-03-31 14:13:14 +0900

    postテーブルに記載

--- database/migrations/2026_03_31_045615_create_posts_table.php
+++ database/migrations/2026_03_31_045615_create_posts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title',200);
+            $table->text('content');
             $table->timestamps();
         });
     }
