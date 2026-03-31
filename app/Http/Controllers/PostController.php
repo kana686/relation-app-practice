a4853f2879f4a90dd673659ad080db524c08b835
kana686 <inui.k.w6@gmail.com>
2026-03-31 15:28:06 +0900

    PostController作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::with('tags')->withCount('comments')->get();
+    }
+    
+    public function show(){
+        $posts = Post::whereHas('tags', fn($q) => $q->where('name', $tagName))->get();
+    }
 }
